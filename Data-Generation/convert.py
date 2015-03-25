6cecf6723b52b91c0d48eb0ecfe5c0c8d43d6b8e
Bedri <bsendir1@binghamton.edu>
2015-03-24 20:17:36 -0400

    updated

--- Data-Generation/convert.py
+++ Data-Generation/convert.py
@@ -8,6 +8,7 @@ import pandas as pd
 out_folder = str(sys.argv[1])
 in_folder = str(sys.argv[1])
 extension = "rrd.xml"
+nodeName = str(sys.argv[2])
 
 def parse(file):
     print file
@@ -28,19 +29,29 @@ def parse(file):
                 data.append(elem.text)
     return file.split('/')[-1].split(".rrd.xml")[0],date,time,data        
 
+
+
 ###########
 first = True
 df= pd.DataFrame()
+icount=0
 for file in os.listdir(in_folder):
     row=[]
     if file.endswith(extension):
-        wfile = open("alldata",'w+')
-        write = csv.writer(wfile)
         column,date,time,data=parse(in_folder+file)
         if(first):
             df["Date"] = date
             df["Time"] = time
             first=False
         df[column]=data
-df.to_csv("test.csv")
+	icount = df[column].count()
+
+mylist=[]
+for x in range(0,icount):
+	mylist.append(nodeName)
+
+df["node"] = mylist
+df.to_csv(nodeName+".csv")
 print df
+
+
