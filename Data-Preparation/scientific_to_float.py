9abe519984d8f76234703b93bf55b7ccb464bcca
Berkay Pehlivan <bpehliv1@binghamton.edu>
2015-03-29 17:21:49 -0400

    correct version

--- Data-Preparation/scientific_to_float.py
+++ Data-Preparation/scientific_to_float.py
@@ -1,1 +1,31 @@
-def convert(dataset):
+
+import numpy as np
+import writetoFile
+import pandas as pd
+
+
+def handle(dataset):
+	
+	count=0
+	row = 0
+	for column in dataset:
+		count +=1
+		if count>1 and column != "Date" and column != "Time" and column != "node":
+			print count,"->",column
+			dataset[column] = map("{0:.64f}".format, np.array(dataset[column],dtype=np.float64))			
+	dataset.to_csv("test.csv",float_format = '{:f}'.format,sep="\t",encoding='utf-8')	
+
+		
+	
+		
+		
+		
+	
+	
+filename="master_dataset.csv"
+data=pd.read_csv("master_dataset.csv",dtype=str)
+
+handle(data)
+
+
+
