87d141f9764d67aa603cb40895021ff39bba8476
Hakan Karadis <hkaradi1@binghamton.edu>
2015-03-30 15:23:24 -0400

    pca.py edited

--- Data-Preparation/pca.py
+++ Data-Preparation/pca.py
@@ -1,4 +1,4 @@
-rom sklearn.decomposition import PCA as sklearnPCA
+from sklearn.decomposition import PCA as sklearnPCA
 
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -33,11 +33,58 @@ ll2 = np.array(ll2)
 
 # 1
 all_samples = np.concatenate((ll, ll2), axis=1)
-assert all_samples.shape == (7, 40), "The matrix has not the dimensions 3x40"
+assert all_samples.shape == (5, 40), "The matrix has not the dimensions 3x40"
+
+# 2
+mean_x = np.mean(all_samples[0,:])
+mean_y = np.mean(all_samples[1,:])
+mean_z = np.mean(all_samples[2,:])
+mean_t = np.mean(all_samples[3,:])
+mean_w = np.mean(all_samples[4,:])
+mean_vector = np.array([[mean_x],[mean_y],[mean_z],[mean_t],[mean_w]])
+
+print('Mean Vector:\n', mean_vector)
+
+# 3 a
+scatter_matrix = np.zeros((5,5))
+for i in range(all_samples.shape[1]):
+    scatter_matrix += (all_samples[:,i].reshape(5,1)\
+         - mean_vector).dot((all_samples[:,i].reshape(5,1) - mean_vector).T)
+print('Scatter Matrix:\n', scatter_matrix)
+
+# 3 b
+cov_mat = np.cov([all_samples[0,:],all_samples[1,:],all_samples[2,:], all_samples[3,:], all_samples[4,:]])
+print('Covariance Matrix:\n', cov_mat)
+
+# 4
+# eigenvectors and eigenvalues for the from the scatter matrix
+eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
+
+# eigenvectors and eigenvalues for the from the covariance matrix
+eig_val_cov, eig_vec_cov = np.linalg.eig(cov_mat)
+
+componentCount=0
+
+for i in range(len(eig_val_sc)):
+    eigvec_sc = eig_vec_sc[:,i].reshape(1,5).T
+    eigvec_cov = eig_vec_cov[:,i].reshape(1,5).T
+    assert eigvec_sc.all() == eigvec_cov.all(), 'Eigenvectors are not identical'
+
+    print('Eigenvector {}: \n{}'.format(i+1, eigvec_sc))
+    print('Eigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
+    if eig_val_sc[i] >= 1: componentCount +=1
+    print('Eigenvalue {} from covariance matrix: {}'.format(i+1, eig_val_cov[i]))
+    print('Scaling factor: ', eig_val_sc[i]/eig_val_cov[i])
+    print(40 * '-')
+
+
+
+print "COMPONENT COUNT ", componentCount
+
 
 sklearn_pca = sklearnPCA(n_components=2)
 sklearn_transf = sklearn_pca.fit_transform(all_samples.T)
-print sklearn_pca
+print sklearn_transf
 ''' 
 plt.plot(sklearn_transf[0:20,0],sklearn_transf[0:20,1],\
      'o', markersize=7, color='blue', alpha=0.5, label='class1')
