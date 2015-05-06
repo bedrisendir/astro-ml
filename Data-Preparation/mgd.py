354d88faaa65a36f3f002790ba92005de5a8156e
Hakan Karadis <hkaradi1@binghamton.edu>
2015-05-06 13:31:16 -0400

    mgd.py edited

--- Data-Preparation/mgd.py
+++ Data-Preparation/mgd.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import math
 from scipy.stats import multivariate_normal
 
 def pcaData(dataframe):
@@ -25,12 +26,26 @@ ll = pd.read_csv(filename)
 ll = np.array(ll)
 #ll[np.isnan(ll)]=0
 
-print len(ll[1])
-print ll[1]
-cov = np.cov(ll[1], rowvar=0)
+print ll.shape
+
+cov = np.cov(ll, rowvar=0)
+mean = np.mean(ll, axis=0)
+
+#cov = np.var(ll, 0)
 cov = np.array(cov)
-mean = np.mean(ll[1], axis=0)
+#mean = np.mean(ll, 0)
 mean = np.array(mean)
+
+print cov
+print mean
+
+#k = mean.shape[0]
+#if len(cov.shape) == 1:
+#	cov = np.diag(cov)
+
+#ll = ll - mean
+#p = (2 * math.pi) ** (-k / 2) * np.linalg.det(cov) ** (-0.5) * np.exp(-0.5 * np.sum(np.dot(ll, np.linalg.pinv(cov)) * ll, 1))
+#print p
 #print cov
 #print mean
 
@@ -38,12 +53,14 @@ mean = np.array(mean)
 #print x
 #print y
 
+#x = np.linspace(0, 5, 10, endpoint=False)
+#y = multivariate_normal.pdf(x, mean=2.5, cov=0.5)
 
-y = multivariate_normal.pdf(ll[1], mean, cov)
+y = multivariate_normal.pdf(ll, mean, cov, allow_singular=True)
 print y
-print len(y)
+#print len(y)
 fig1 = plt.figure()
 ax = fig1.add_subplot(111)
-ax.plot(ll[1], y)
+ax.plot(ll, y)
 #y = multivariate_normal.pdf(ll, mean, cov)
 #plt.plot(x,y,'x'); plt.axis('equal'); plt.show()
